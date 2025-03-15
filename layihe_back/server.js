75d3f501aad713222ee83478ae0f320473903b18
Azer Dunyamaliyev <147849064+Azer-Dunyamaliyev@users.noreply.github.com>
2025-03-15 13:30:14 +0400

    Update server.js

--- layihe_back/server.js
+++ layihe_back/server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 // CORS
 const corsOptions = {
-  origin: "*",
+  origin: "https://koton-blond.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Authorization'],
