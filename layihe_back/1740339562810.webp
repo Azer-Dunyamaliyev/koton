e9e5f52fa7ebfd0ec182895479c41af86aef2c9e
Azer Dunyamaliyev <147849064+Azer-Dunyamaliyev@users.noreply.github.com>
2026-07-02 14:09:40 +0400

    Delete layihe_back directory

--- layihe_back/1740339562810.webp
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addSubscriber } from "../controller/controller.js";
-
-const router = express.Router();
-
-router.post("/", addSubscriber); 
-
-export default router;
--- layihe_back/auth (1).js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import uploadMiddleware from "../middleware/uploadMiddelware.js";
-import { uploadImage } from "../controller/controller.js";
-
-const router = express.Router();
-
-router.post("/", uploadMiddleware.single("image"), uploadImage);
-
-export default router;
